8c7e2b03428334fa4b3b98e1cb4353fef1ccf55b
omidazadi <azadiomid80@gmail.com>
2022-07-01 01:18:26 +0430

    Parser added.

--- datatypes.rkt
+++ datatypes.rkt
@@ -2,14 +2,24 @@
 
 (require (lib "eopl.ss" "eopl"))
 
-(provide stmt stmt? assign-stmt side-effect-stmt
+(provide stmt stmt? assign-stmt side-effect-stmt return-value-stmt return-novalue-stmt global-stmt pass-stmt continue-stmt break-stmt def-stmt default-stmt if-stmt for-stmt
     expr expr? num-expr app-expr ident-expr
     val val? num-val proc-val none-val
     force-num force-proc)
 
 (define-datatype stmt stmt?
-    (assign-stmt (name symbol?) (value expr?))
-    (side-effect-stmt (e expr?)))
+    (assign-stmt (name expr?) (value expr?))
+    (side-effect-stmt (e expr?))
+    (return-value-stmt (e expr?))
+    (return-novalue-stmt)
+    (global-stmt (e expr?))
+    (pass-stmt)
+    (continue-stmt)
+    (break-stmt)
+    (def-stmt (name expr?) (params list?) (statements list?))
+    (default-stmt (name expr?) (value expr?))
+    (if-stmt (condition expr?) (statements list?) (else-block list?))
+    (for-stmt (counter expr?) (count-set list?) (statements list?)))
 
 (define-datatype expr expr?
     (num-expr (v number?))
