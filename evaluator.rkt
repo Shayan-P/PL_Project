cc344992e6a4ee6aaffc345bcf39d4539961b907
arshia.soltani2 <arshia.soltani2@gmail.com>
2022-07-11 17:13:45 +0430

    evaluate function calls, math

--- evaluator.rkt
+++ evaluator.rkt
@@ -3,15 +3,35 @@
 (require (lib "eopl.ss" "eopl"))
 (require "./datatypes.rkt")
 
-(provide eval-stmts prelude-env benv-lookup)
+(provide eval-stmts prelude-env benv-lookup )
+
+(define debug (lambda (s) (let [(t1 (pretty-print `************))(t2 (our-pretty-print s))(t3 (pretty-print `*********))] s)))
+(define pipe-none-val (lambda (s)  (none-val ) ))
+(define (is-none? v)
+    (cases val v
+        (none-val () #t )
+        (else #f )))
+
+
+(define our-pretty-print (lambda (s)
+ (if (procedure? s) (display s) (display s ))))
 
 (define (eval-stmts env l)
-    (if (null? l) 'end (let ([ignore (eval-stmt env (car l))]) (eval-stmts env (cdr l)))))
+    (if (null?  l) 'end (let [(ignore (eval-stmt env (car l)))] (eval-stmts env (cdr l)))))
+
+(define (eval-stmts-with-return env l)
+    (if (null?  l) (none-val) (let [(val (eval-stmt env (car l)))] (if (is-none? val) (eval-stmts-with-return env (cdr l)) val))))
+
 (define (eval-stmt env s)
     (cases stmt s
-        (side-effect-stmt (e) (eval-expr env e))
-        (assign-stmt (name val) (benv-extend! env name (eval-expr env val)))
+        (side-effect-stmt (e) (pipe-none-val(eval-expr env e)))
+        (assign-stmt (name val) (pipe-none-val(benv-extend! env name (eval-expr env val))))
+        (def-stmt (name arg body) (pipe-none-val(benv-extend! env name (function-builder body))))
+        (return-value-stmt (e) (eval-expr env e))
         (else (error 'TODO))))
+
+(define (function-builder body) 
+    (proc-val (lambda (env dummy) (let [] (eval-stmts-with-return env body)))))
 
 (define (env-lookup env v) (env v))
 (define (env-extend env name val) (lambda (n) (if (eq? name n) val (env n))))
@@ -29,7 +49,11 @@
     (list
         (list 'print (proc-val (lambda (e arg) (let ([ignore (pretty-print arg)]) none-val))))
         (list '$mul (pnf2 *))
-        (list '$plus (pnf2 +)))))
+        (list '$plus (pnf2 +))
+        (list `$pow (pnf2 expt))
+        (list `$minus (pnf2 -))
+        (list `$div (pnf2 /))
+        (list `$dummy (pnf2 *)))))
 
 (define (eval-expr env e)
     (cases expr e
--- main.rkt
+++ main.rkt
@@ -4,16 +4,26 @@
 (require "./evaluator.rkt")
 
 (define (main) '())
+(define debug (lambda (s) (let [(t1 (pretty-print `************))(t2 (pretty-print s))(t3 (pretty-print `*********))] s)))
 
 
 (let [(parser-res (str-to-sexp "
-    print(1);
+    def tt():
+        a=5;
+        print(a);
+        a=a+1;
+        print(7);
+        def g():
+            print(8);
+            ;
+        return g;
+        ;
     a = 2;
-    print(a * 3 + 2);
-    print(3);
+    b=tt();
+    print(b());
 "))
 (env (box prelude-env))] (let
-[(ignore (eval-stmts env parser-res))] 'finished))
+[ (ignore (eval-stmts env (debug parser-res)))] 'finished))
 
 ;(let ((parser-res (str-to-sexp "
 ;    def f():
