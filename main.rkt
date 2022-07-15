af37d70a5a64bf6d649a261f73684db8292ab57a
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-07-15 13:36:10 +0430

    fix main

--- main.rkt
+++ main.rkt
@@ -5,14 +5,14 @@
 
 ;;; (define (main) '())
 ; switch the comment for debugging
-(define debug (lambda (s) (let [(t1 (pretty-print `************))(t2 (pretty-print s))(t3 (pretty-print `*********))] s)))
-;;; (define debug (lambda (s) s))
+;(define debug (lambda (s) (let [(t1 (pretty-print `************))(t2 (pretty-print s))(t3 (pretty-print `*********))] s)))
+(define debug (lambda (s) s))
 
-;;; (define (evaluate f) (let [(parser-res (str-to-sexp (file->string f)))
-;;; (env (box prelude-env))] (let
-;;; [ (ignore (eval-stmts env env (debug parser-res)))] 'finished)))
+(define (evaluate f) (let [(parser-res (str-to-sexp (file->string f)))
+(env (box prelude-env))] (let
+[ (ignore (eval-stmts env env (debug parser-res)))] 'finished)))
 
-;;; (evaluate (car (vector->list (current-command-line-arguments))))
+(evaluate (car (vector->list (current-command-line-arguments))))
 
 ;(let ((parser-res (str-to-sexp "
 ;    def f():
@@ -25,18 +25,18 @@
 ;    print(a);
 ;"))) parser-res)
 
-(let [(parser-res (str-to-sexp "
-    def sum(l=[], f=None):
-        a = 0;
-        for x in l:
-            a = x + a;
-            ;
-        return a;
-        ;
+;; (let [(parser-res (str-to-sexp "
+;;     def sum(l=[], f=None):
+;;         a = 0;
+;;         for x in l:
+;;             a = x + a;
+;;             ;
+;;         return a;
+;;         ;
 
-    print(sum([1, 2, 3]));
-    print(sum([1, 10, 100]));
-    print(sum([]));
-"))
-(env (box prelude-env))] (let
-[ (ignore (eval-stmts env env (debug parser-res)))] 'finished))
+;;     print(sum([1, 2, 3]));
+;;     print(sum([1, 10, 100]));
+;;     print(sum([]));
+;; "))
+;; (env (box prelude-env))] (let
+;; [ (ignore (eval-stmts env env (debug parser-res)))] 'finished))
